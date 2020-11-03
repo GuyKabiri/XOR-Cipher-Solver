58949df6ed7a583fde9388520de84cb110e3b8cf
GuyK <guyk1994@gmail.com>
2020-11-03 18:18:38 +0200

    Add comments to HashTable.

--- XOR Cipher Solver/HashTable.c
+++ XOR Cipher Solver/HashTable.c
@@ -5,17 +5,19 @@
 
 void init_hashtable(HashTable* ht)
 {
-	for (int i = 0; i < HASHTABLE_SIZE; i++)
+	for (int i = 0; i < HASHTABLE_SIZE; i++)	//	iterate over the array and initiate every linked list
 		init_list(&ht->table[i]);
 }
 
+//	add a new word to the table
+//	return the key of the word if added, otherwise return -1
 int add_word(HashTable* hs, char* word)
 {
-	int key = hash_key(word);
-
-	return add_char_node(&hs->table[key], word) ? key : -1;
+	int key = hash_key(word);									//	get the hash key of a word
+	return add_char_node(&hs->table[key], word) ? key : -1;		//	add the word to the table, and return the key of the word if add, otherwise return -1
 }
 
+//	generate hash key for a word
 int hash_key(char* word)
 {
 	int key = 0;
@@ -27,10 +29,12 @@ int hash_key(char* word)
 	return key;
 }
 
+//	check if a word exists in the table
+//	return 1 if exists, otherwise 0
 int is_exists_hashtable(HashTable* ht, char* word)
 {
-	int key = hash_key(word);
-	return is_exists_linkedlist(&ht->table[key], word);
+	int key = hash_key(word);								//	generate a hash key for the word
+	return is_exists_linkedlist(&ht->table[key], word);		//	checks if the word exists in that linked list
 }
 
 void print_hashtable(HashTable* ht)
